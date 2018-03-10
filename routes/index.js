f1e875f3ab1e95ba41b3e6c6a501f89b340d3bb6
Matt Burman <matt@mattburman.com>
2018-03-10 11:38:09 +0000

    Modify the teams rendering to simple list

--- routes/index.js
+++ routes/index.js
@@ -5,72 +5,64 @@ const YAML = require('yamljs');
 const GITHUB_TOKEN = process.env.GITHUB_TOKEN;
 if (!GITHUB_TOKEN) throw new Error("Supply GitHub token");
 
-const graphql = `{
- organization(login:"hacksheffield") {		
-    avatarUrl
-    databaseId
-    repository(name:"people"){
-      name
-      object(expression:"master:_data/people.yml"){
-      	repository{
-          object(expression:"master:_config/team-blacklist.json"){
-            ... on Blob {
-              text
-            }
-          }
-        }
-        ... on Blob {
-          text
-        }
-      }
-    }
-    teams (first: 100){
-      edges {
-        node{
-          description
-          teamsUrl
-          url
-          slug
-          combinedSlug
-          name
-          members (first: 100){
-            edges {
-              node {
-                name
-                bio
-                url
-                login
-                location
-                bioHTML
-                email
-                avatarUrl
-                websiteUrl
-                isCampusExpert
-              }
-            }
-          }
-        }
+const getpeople = `{
+  repository(owner:"mattburman", name:"hackmed-people") {
+    object(expression:"master:people.yml") {
+      ... on Blob {
+      	text
       }
     }
   }
 }`;
 
+const getdetails = `{
+  search(first:100, type:USER, query:"{{USERS}}"){
+    nodes{
+      ...on User {
+        name
+        login
+        avatarUrl
+        websiteUrl
+      }
+    }
+  }
+}`;
+
+const headers = { 'Authorization': `Bearer ${GITHUB_TOKEN}` };
+
 /* GET home page. */
 router.get('/', (req, res, next) => {
-  axios.post('https://api.github.com/graphql', { query: graphql }, { headers: {'Authorization': `Bearer ${GITHUB_TOKEN}`}})
+  axios.post('https://api.github.com/graphql', { query: getpeople }, { headers })
     .then((graphqlRes) => {
       console.log(graphqlRes.data);
-      const teams = graphqlRes.data.data.organization.teams.edges.map(edge => edge.node);
       const people = {};
-      YAML.parse(graphqlRes.data.data.organization.repository.object.text).forEach(person => {
+      YAML.parse(graphqlRes.data.data.repository.object.text).forEach(person => {
         people[person.github] = person;
       });
-      const teamBlacklist = JSON.parse(graphqlRes.data.data.organization.repository.object.repository.object.text);
 
-      console.log(people);
-      teams.forEach(team => team.members = team.members.edges.map(edge => edge.node));
+      const userQueryString = Object.keys(people)
+        .reduce((q, githubLogin) => `${q} user:${githubLogin}`, '');
+
+      console.log(userQueryString);
+
+      console.log(getdetails.replace('{{USERS}}', userQueryString));
+      axios.post('https://api.github.com/graphql', { query: getdetails.replace('{{USERS}}', userQueryString) }, { headers })
+        .then((graphqlRes) => {
+          graphqlRes.data.data.search.nodes.forEach(person => {
+            Object.keys(person).forEach(key => {
+              people[person.login][key] = person[key];
+              people[person.login].thing = 'test';
+            });
+          });
+          console.log(people);
+          res.render('index', { title: 'Teams', people });
+      }).catch(err => {
+        console.error(err);
+        res.render('error', {});
+      });
+
       // const people = graphqlRes.data.data.organization.teams.edges.map(edge => edge.node)[0].members.edges.map(edge => edge.node);
-      res.render('index', { title: 'Teams', teams: teams, people, teamBlacklist });
+      // res.render('index', { title: 'Teams', people });
     })
     .catch((err) => {
       console.log(err);
